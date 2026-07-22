cdf1fe4cf81422ccf882dcf847adc0bb50993465
Xavier Riva-Pommarede <xavier.pommarede@gmail.com>
2026-07-22 11:00:43 +0200

    detect when relayMeasureScreen is displayed

--- src/UI/screens/relaymeasurescreen.cpp
+++ src/UI/screens/relaymeasurescreen.cpp
@@ -1,14 +1,40 @@
 #include "relaymeasurescreen.h"
+#include "navigator.h"
 #include "ui_relaymeasurescreen.h"
+#include <QDebug>
+#include <QShowEvent>
+#include <memory>
 
-RelayMeasureScreen::RelayMeasureScreen(QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::RelayMeasureScreen)
-{
+RelayMeasureScreen::RelayMeasureScreen(QWidget* parent)
+    : QWidget(parent), ui(new Ui::RelayMeasureScreen) {
     ui->setupUi(this);
 }
 
-RelayMeasureScreen::~RelayMeasureScreen()
-{
+RelayMeasureScreen::~RelayMeasureScreen() {
     delete ui;
+}
+
+void RelayMeasureScreen::showEvent(QShowEvent* event) {
+    QWidget::showEvent(event);
+
+    // Check navigator exchange data for this screen
+    auto data = Navigator::instance().getScreenExchangeData(Navigator::RelayMeasure_screen);
+    if (data) {
+        auto strptr = std::static_pointer_cast<QString>(data);
+        if (strptr) {
+            emit navigatedTo(*strptr);
+            onNavigatedTo(*strptr);
+            return;
+        }
+    }
+
+    emit navigatedTo(QString());
+    onNavigatedTo(QString());
+}
+
+void RelayMeasureScreen::onNavigatedTo(const QString& path) {
+    Q_UNUSED(path);
+    // Default handler: override or connect to navigatedTo() to react when shown
+
+    qDebug() << "RelayMeasureScreen navigated to path:" << path;
 }
--- src/UI/screens/relaymeasurescreen.h
+++ src/UI/screens/relaymeasurescreen.h
@@ -1,22 +1,33 @@
 #ifndef RELAYMEASURESCREEN_H
 #define RELAYMEASURESCREEN_H
 
+#include <QString>
 #include <QWidget>
+
+class QShowEvent;
 
 namespace Ui {
 class RelayMeasureScreen;
 }
 
-class RelayMeasureScreen : public QWidget
-{
+class RelayMeasureScreen : public QWidget {
     Q_OBJECT
 
 public:
-    explicit RelayMeasureScreen(QWidget *parent = nullptr);
+    explicit RelayMeasureScreen(QWidget* parent = nullptr);
     ~RelayMeasureScreen();
 
+public slots:
+    void onNavigatedTo(const QString& path);
+
+signals:
+    void navigatedTo(const QString& path);
+
+protected:
+    void showEvent(QShowEvent* event) override;
+
 private:
-    Ui::RelayMeasureScreen *ui;
+    Ui::RelayMeasureScreen* ui;
 };
 
 #endif // RELAYMEASURESCREEN_H
